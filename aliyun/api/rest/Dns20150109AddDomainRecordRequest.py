0a42e5b53f7e4b74fbdb4628d6c2504ec95d1e8a
janncker <janncker@gmail.com>
2019-03-20 22:09:11 +0800

    fix  more MissingTimestamp error

--- aliyun/api/rest/Dns20150109AddDomainRecordRequest.py
+++ aliyun/api/rest/Dns20150109AddDomainRecordRequest.py
@@ -2,13 +2,16 @@
 Created by auto_sdk on 2015.09.21
 '''
 from aliyun.api.base import RestApi
+import datetime
+
 class Dns20150109AddDomainRecordRequest(RestApi):
-	def __init__(self,address,host="adascore.xyz", domain='dns.aliyuncs.com',port=80):
+	def __init__(self,address,host="adascore.com", domain='dns.aliyuncs.com',port=80):
 		RestApi.__init__(self,domain, port)
 		self.Line = "default"
 		self.Priority = None
 		self.RR = "@"
 		self.DomainName=host
+                self.Timestamp = datetime.datetime.utcnow().isoformat()
                 self.TTL = None
 		self.Type = "A"
 		self.Value = address
--- aliyun/api/rest/Dns20150109DescribeDomainRecordInfoRequest.py
+++ aliyun/api/rest/Dns20150109DescribeDomainRecordInfoRequest.py
@@ -2,9 +2,12 @@
 Created by auto_sdk on 2015.09.18
 '''
 from aliyun.api.base import RestApi
+import datetime
+
 class Dns20150109DescribeDomainRecordInfoRequest(RestApi):
 	def __init__(self,recordid,domain='dns.aliyuncs.com',port=80):
 		RestApi.__init__(self,domain, port)
+                self.Timestamp = datetime.datetime.utcnow().isoformat()
 		self.RecordId = recordid
 
 	def getapiname(self):
